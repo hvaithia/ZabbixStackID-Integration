2d0a69c28bc863a4a7d12e0272bc37c76b9c8404
hvaithia <hariharan.v88@gmail.com>
2014-10-18 14:30:55 -0700

    stackID file updated

--- stackID.py
+++ stackID.py
@@ -13,17 +13,9 @@ print "Connected to Zabbix API Version %s" % zapi.api_version()
 def get_zabbix_hosts():
   host_list = []
   for h in zapi.host.get(output='extend'):
-    #print h['hostid'], h['host']
     b=h['host'].encode("utf-8")
     host_list.append(b)
-  #host_list=[x.lower() for x in host_list]
   return host_list
-
-  # Operations of hosts
-  #print "List of hosts:"
-  #for hosts in host_list:
-  #  print hosts
-  #print "Size of the host list is: %d" %(len(host_list))
 
 def get_host_groups():
   host_groups=[]
@@ -40,14 +32,7 @@ def parse_stackID(csvfile):
     y = line.strip().split(",")
     List.append(y)
 
-    #tuple method
-    #tuple(filter(None, line.strip().split(',')))
   return List
-
-  #Accessing each elements in Lists of List
-  #for lists in List:
-    #for elements in lists:
-      #print elements
 
 def create_hostgroups(List_zabbix,List_csv):
 
@@ -61,20 +46,15 @@ def create_hostgroups(List_zabbix,List_csv):
   Host_csv=[x.lower() for x in Host_csv]
   stack_csv= [x.lower() for x in stack_csv]
   List_zabbix= [x.lower() for x in List_zabbix]
-  print List_zabbix
-  print "-----------------hostgroups Host_csv stack_csv----------------"
-  print hostgroups
+  #print List_zabbix
+  #print "-----------------hostgroups Host_csv stack_csv----------------"
+  #print hostgroups
   #print Host_csv
   #print stack_csv
-  print "-----------------hostgroups Host_csv stack_csv----------------"
+  #print "-----------------hostgroups Host_csv stack_csv----------------"
   
 
   for index, host_zabbix in enumerate(List_zabbix):
-    #host_zabbix=host_zabbix_original.strip().split(".")[0]
-    #if host_zabbix in [item[0] for item in Host_csv] and [item[1] != '' for item in Host_csv] and [item[1] not in hostgroups]:
-    #print "test host_zabbix -------"
-    #print host_zabbix.strip().split(".")[0]
-    #print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     if (host_zabbix in Host_csv) and (stack_csv[Host_csv.index(host_zabbix)]) != '': # The and condition is not reqd --- and (stack_csv[Host_csv.index(host_zabbix)] not in hostgroups):
      ##########if (stack_csv[Host_csv.index(host_zabbix)]) != '':
       #create host groups with item[1] value and add item[0] as host in that group
@@ -110,11 +90,10 @@ def create_hostgroups(List_zabbix,List_csv):
     #elif:
       #check to see if the host is added to the host group. If added exit else below step
       #Host group is already created. Just add host to the host group
-    #else:
-      #Do nothing. Exit
+    else:
+      sys.exit()
       
 def main():
-  #get the csv file name and location as arg1
   args=sys.argv[1:]
 
   if not args:
@@ -128,13 +107,9 @@ def main():
     sys.exit(1)
   List_zabbix=get_zabbix_hosts()
   print "------List_zabbix List_csv------"
-  #print List_zabbix
   List_csv=parse_stackID(args[0])
   #print List_csv
   print "No of items in the list is: %d" %(len(List_csv))
-  #for n,items in enumerate(List_csv):
-    #for each_item in items:
-    #print "Index is %d. Items in the list_csv are %s %s " %(n,items[0],items[2])
   print "------List_zabbix List_csv------"
   # Do A tolower on both lists before passing
 
